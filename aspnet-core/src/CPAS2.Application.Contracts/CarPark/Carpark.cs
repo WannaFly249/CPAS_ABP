b2768c4a9c0c12459b91ae1a60546281311ab83c
Tien Tran <tientm@smartosc.com>
2021-03-11 10:24:19 +0700

    Add carparkservice

---
+++ aspnet-core/src/CPAS2.Application.Contracts/CarPark/Carpark.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CPAS2.CarPark
+{
+    public class Carpark
+    {
+        public List<CarparkDt> Items { get; set; }
+    }
+
+    public class CarparkIf
+    {
+        [JsonProperty("carpark_info")]
+        public List<CarparkDetail> CarparkInfo { get; set; }
+        [JsonProperty("carpark_number")]
+        public string CarparkNumber { get; set; }
+        [JsonProperty("update_datetime")]
+        public string UpdateDatetime { get; set; }
+    }
+
+    public class CarparkDetail
+    {
+        [JsonProperty("total_lots")]
+        public string TotalLots { get; set; }
+        [JsonProperty("lot_type")]
+        public string LotType { get; set; }
+        [JsonProperty("lots_available")]
+        public string LotsAvailable { get; set; }
+    }
+
+    public class CarparkDt
+    {
+        [JsonProperty("carpark_data")]
+        public List<CarparkIf> CarparkData { get; set; }
+        public string TimeStamp { get; set; }
+    }
+}
---
+++ aspnet-core/src/CPAS2.Application/CarPark/CarparkService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPAS2.CarPark
+{
+    public class CarparkService : CPAS2AppService, ICarparkService
+    {
+        #region Contructor
+
+        public CarparkService()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Method
+        [HttpGet]
+        public async Task<List<CarparkIf>> CheckCarparkAvailability(string dateTime)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create($"https://api.data.gov.sg/v1/transport/carpark-availability?date_time={dateTime}");
+            webRequest.Method = "GET";
+            webRequest.ContentType = "application/json";
+            HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
+            using var streamReader = new StreamReader(webResponse.GetResponseStream());
+            var jsonResult = await streamReader.ReadToEndAsync();
+            var result = JsonConvert.DeserializeObject<Carpark>(jsonResult);
+            if (result == null) return null;
+            return result.Items.FirstOrDefault()?.CarparkData.Take(20).ToList();
+        }
+        #endregion
+    }
+}
