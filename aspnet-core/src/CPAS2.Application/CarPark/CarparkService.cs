6cbc25d8894d19c607ede9d23037bcff17e0752d
Tien Tran <tientm@smartosc.com>
2021-03-12 10:42:28 +0700

    Clean code

--- aspnet-core/src/CPAS2.Application/CarPark/CarparkService.cs
+++ aspnet-core/src/CPAS2.Application/CarPark/CarparkService.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CPAS2.CarPark
@@ -22,18 +20,18 @@ namespace CPAS2.CarPark
         #endregion
 
         #region Public Method
+
         [HttpGet]
         public async Task<List<CarparkIf>> CheckCarparkAvailability(string dateTime)
         {
-            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create($"https://api.data.gov.sg/v1/transport/carpark-availability?date_time={dateTime}");
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(CPAS2Constant.CarparkApiUrl);
             webRequest.Method = "GET";
             webRequest.ContentType = "application/json";
             HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
             using var streamReader = new StreamReader(webResponse.GetResponseStream());
             var jsonResult = await streamReader.ReadToEndAsync();
             var result = JsonConvert.DeserializeObject<Carpark>(jsonResult);
-            if (result == null) return null;
-            return result.Items.FirstOrDefault()?.CarparkData.Take(20).ToList();
+            return result?.Items.FirstOrDefault()?.CarparkData.Take(CPAS2Constant.NumberOfLots).ToList();
         }
         #endregion
     }
---
+++ aspnet-core/src/CPAS2.Domain.Shared/CPAS2Constant.cs
@@ -0,0 +1,9 @@
+﻿
+namespace CPAS2
+{
+    public static class CPAS2Constant
+    {
+        public const string CarparkApiUrl = $"https://api.data.gov.sg/v1/transport/carpark-availability?date_time={dateTime}";
+        public const int NumberOfLots = 20;
+    }
+}
